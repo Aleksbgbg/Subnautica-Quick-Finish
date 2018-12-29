868a50e90dbf71d06f0cf1f354a1c4e85b96a18c
Aleksbgbg <kozichka01@abv.bg>
2018-12-29 22:55:54 +0000

    [NeptuneRocketConstructor] Add class to construct pieces of the Neptune rocket.

---
+++ Subnautica Quick Finish/Components/NeptuneRocketConstructor.cs
@@ -0,0 +1,32 @@
+﻿namespace Subnautica.Quick.Finish.Components
+{
+    using UnityEngine;
+
+    internal class NeptuneRocketConstructor : MonoBehaviour
+    {
+        private RocketConstructor _rocketConstructor;
+
+        internal void ConstructNextNeptuneRocketPiece()
+        {
+            if (_rocketConstructor == null)
+            {
+                // Global search call required as RocketConstructor host is unknown.
+                // TODO: Optimise with GetComponent call on known host object.
+                _rocketConstructor = FindObjectOfType<RocketConstructor>();
+
+                // In case the user ran the script
+                // before the Neptune Launch Platform
+                // was built.
+                if (_rocketConstructor == null)
+                {
+                    return;
+                }
+            }
+
+            // Successive calls to StartRocketConstruction
+            // will actually advance the current rocket
+            // stage and build the next piece.
+            _rocketConstructor.StartRocketConstruction();
+        }
+    }
+}
