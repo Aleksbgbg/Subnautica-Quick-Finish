5779d9f587b474e803784ecbd48cc0ac7d572c05
Aleksbgbg <kozichka01@abv.bg>
2018-12-29 22:15:33 +0000

    [Loader] Add Loader class to create a HookTarget GameObject.

---
+++ Subnautica Quick Finish/Loader.cs
@@ -0,0 +1,17 @@
+﻿namespace Subnautica.Quick.Finish
+{
+    using UnityEngine;
+
+    internal static class Loader
+    {
+        // Methods in this class are private as the injector does not
+        // need public access to inject.
+        // Methods are therefore less likely to be called by accident.
+
+        private static void Load()
+        {
+            GameObject hookTarget = new GameObject();
+            Object.DontDestroyOnLoad(hookTarget);
+        }
+    }
+}
